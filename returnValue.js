e7c39b3b04a3839e30d9114e9bbeabba07706725
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-08-11 16:52:35 +0530

    returnValue.js is added

---
+++ returnValue.js
@@ -0,0 +1,8 @@
+//Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+let processed = processArg(7);
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+console.log(processed);
