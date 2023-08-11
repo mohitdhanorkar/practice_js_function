80c44765b0d36e05cb58a5d06d34c28218644df1
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-08-11 16:22:19 +0530

    undefinedValue.js file is updated

---
+++ undefinedValue.js
@@ -0,0 +1,11 @@
+let sum = 4;
+
+function addThree() {
+  return sum = sum + 3;
+}
+function addFive()
+{
+  return sum = sum +5; // value of sum is 7
+}
+console.log(addThree());
+console.log(addFive());
