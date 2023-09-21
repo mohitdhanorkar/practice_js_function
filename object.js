910b62bd5b5da46928764be0986c035c1eb701fc
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-09-22 00:38:19 +0530

    add object.js

--- object.js
+++ object.js
@@ -1,9 +1,22 @@
 //object-- Javascript object is an physical entity which is having type and properties.
 
-let person= {
-    firstName: "Mohit",
-    lastName: "Dhanorkar",
-    age : 25,
-    colour: "White"
-};
-console.log(person.firstName)
+// let person= {
+//     firstName: "Mohit",
+//     lastName: "Dhanorkar",
+//     age : 25,
+//     colour: "White"
+// };
+// console.log(person.firstName)
+
+//using this keyword
+
+const person1={
+    firstName: "Rohit",
+    lastName: "Varma",
+    DOB: 23/3/1999,
+    age: 25,
+    fullDetails: function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+console.log(person1.fullDetails());
