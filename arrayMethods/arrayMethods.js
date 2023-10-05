0fdcb59ae92411a84d7ce9fad5422b62519f3d71
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-10-05 17:07:17 +0530

    add array.map

--- arrayMethods/arrayMethods.js
+++ arrayMethods/arrayMethods.js
@@ -1,3 +1,7 @@
 // Array.lenght()
 const alphabates = ["A", "B", "C", "D", "E"];
 console.log(alphabates.length);
+
+// Array.map()
+const number1 = [4, 25, 8, 12];
+console.log(number1.map(Math.sqrt));
