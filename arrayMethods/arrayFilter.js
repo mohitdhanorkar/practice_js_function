fb4bebf792a9b1c666f687a1a5fc4d045f96db7b
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-10-05 11:06:51 +0530

    add array.filter

--- arrayMethods/arrayFilter.js
+++ arrayMethods/arrayFilter.js
@@ -3,3 +3,13 @@ function checkAdult(age){
     return age>=18;
 }
 console.log(ages.filter(checkAdult)) // it will filter the age which is less than 18 
+
+// ------------------------------------------------------------------------------------------------
+
+const number = [12, 19, 14, 22, 97];
+function evenNumber(even){
+    return even % 2;
+}
+console.log(number.filter(evenNumber)) // it will filter out the numbers which is divisible by 2 
+
+// -------------------------------------------------------------------------------------------------
