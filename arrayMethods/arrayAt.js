7248b9234e6a4e8230220e53617fefc59a8a4f1f
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-10-05 15:58:55 +0530

    add arrayAt.js

--- arrayMethods/arrayAt.js
+++ arrayMethods/arrayAt.js
@@ -1,3 +1,9 @@
 const fruits = ["Orange", "Apple", "Kiwi", "Banana"];
 let fruit = fruits.at(2)
 console.log(fruit);
+//----------------------------------------------------------------
+
+const person = ["Mohit", "Rohit", "Raju", "Pranav"];
+console.log(person.at(0));
+
+//----------------------------------------------------------------
