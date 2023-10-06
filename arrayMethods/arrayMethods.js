b2fb5c4b2fe5cbb6ed78d8a1d3f86cf95a90f3d1
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-10-06 11:22:57 +0530

    add array.push

--- arrayMethods/arrayMethods.js
+++ arrayMethods/arrayMethods.js
@@ -1,10 +1,15 @@
-// Array.lenght()
-const alphabates = ["A", "B", "C", "D", "E"];
-console.log(alphabates.length);
+// // Array.lenght()
+// const alphabates = ["A", "B", "C", "D", "E"];
+// console.log(alphabates.length);
 
 // Array.map()
-const number1 = [4, 25, 8, 12];
-console.log(number1.map(myFunction));
-function myFunction(num){
-    return num*10;
-}
+// const number1 = [4, 25, 8, 12];
+// console.log(number1.map(myFunction));
+// function myFunction(num){
+//     return num*10;
+// }
+
+// Array.push()
+const object = ["Wood", "Pen", "Rubber", "Sharpner"];
+object.push("Steel", "Buds");
+console.log(object);
